2aa3745c638fe0df990b24ab30e5436cef432e24
Alexandre Kaimer <alekaimer@gmail.com>
2023-03-01 11:45:02 -0300

    feat: adds basic token files

---
+++ packages/tokens/src/line-heights.ts
@@ -0,0 +1,6 @@
+export const lineHeights = {
+  shorter: '125%', // base to headding (1.25)
+  short: '140%',
+  base: '160%', // base to multiline web (1.6)
+  tall: '180%',
+}
---
+++ packages/tokens/src/radii.ts
@@ -0,0 +1,10 @@
+// only absolutely values
+
+export const radii = {
+  px: '1px',
+  xs: '4px',
+  sm: '6px',
+  md: '8px',
+  lg: '16px',
+  full: '99999px', // full circle
+}
